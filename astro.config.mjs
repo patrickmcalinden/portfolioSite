c2a11db20c77fc9f1e5a3e44de067db841186dc0
Patrick <patrickmcalinden226@gmail.com>
2023-11-06 01:24:54 -0500

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: 'https://patrickmcalinden.github.io',
-  base: '/my-repo',
+  base: '/portfolioSite',
   integrations: [tailwind(),  sitemap()]
 });
